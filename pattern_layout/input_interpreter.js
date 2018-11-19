7d035eb8f9dc402a492e6ab35d3e43341f29e592
Buzz Tilford <tilford20@gmail.com>
2018-11-19 13:39:22 -0800

    added pattern control pge

---
+++ pattern_layout/input_interpreter.js
@@ -0,0 +1,109 @@
+/* key
+frequency: note octave Bn1 Cn4 D#2. . .
+volume: 0-f
+envelope_direction: 0-1 (down/up)
+envelope: 0-f
+use_length: 0 - 1
+length: 01 - 64
+pulse_length: 0 - 3
+sweep_direction: 0 - 1
+sweep_shift_numner: 0 - 8
+sweep_speed: 0 - 8
+*/
+var interpret_channel_1 = function(){
+    pulse_1_input = patterns[1][current_patterns[1]][current_step]
+    if (pulse_1_input == "-") return;
+
+    console.log(pulse_1_input.substring(0, 3));
+    console.log(square_wave_note_to_memory_value[pulse_1_input.substring(0, 3)]);
+    pulse_1.frequency_memory = square_wave_note_to_memory_value[pulse_1_input.substring(0, 3)];
+    pulse_1.volume = parseInt(pulse_1_input[3], 16);
+    pulse_1.envelope_direction = parseInt(pulse_1_input[4], 2);
+    pulse_1.envelope_number = parseInt(pulse_1_input[5], 16);
+    pulse_1.use_length = parseInt(pulse_1_input[6], 2);
+    pulse_1.sound_length_counter = parseInt(Number(pulse_1_input[7] + "" + pulse_1_input[8], 64) + 1);
+    pulse_1.pulse_width = parseInt(pulse_1_input[9], 4);
+
+
+    pulse_1.sweep_direction = parseInt(pulse_1_input[10], 2);
+    pulse_1.sweep_shift_number = parseInt(pulse_1_input[11], 8);
+    pulse_1.sweep_shift_time = parseInt(pulse_1_input[12], 8);
+
+}
+/* key
+frequency: note octave Bn1 Cn4 D#2. . .
+volume: 0-f
+envelope_direction: 0-1 (down/up)
+envelope: 0-f
+use_length: 0 - 1
+length: 01 - 64
+pulse_length: 0 - 3
+*/
+var interpret_channel_2 = function(){
+    pulse_2_input = patterns[2][current_patterns[2]][current_step]
+
+    if (pulse_2_input == "-") return;
+
+    pulse_2.frequency_memory = square_wave_note_to_memory_value[pulse_2_input.substring(0, 3)];
+    pulse_2.volume = parseInt(pulse_2_input[3], 16);
+    pulse_2.envelope_direction = parseInt(pulse_2_input[4], 2);
+    pulse_2.envelope_number = parseInt(pulse_2_input[5], 16);
+    pulse_2.use_length = parseInt(pulse_2_input[6], 2);
+    pulse_2.sound_length_counter = parseInt(Number(pulse_2_input[7] + "" + pulse_2_input[8], 64) + 1);
+    pulse_2.pulse_width = parseInt(pulse_2_input[9], 4);
+
+}
+
+/* key
+frequency: note octave Cn1 Cn4 D#2. . .
+volume: 0-3
+use_length: 0 - 1
+length: 01 - 256
+shape: 0-f  (0-2 built in currently)
+*/
+var interpret_channel_3 = function(){
+    wave_3_input = patterns[3][current_patterns[3]][current_step]
+    if (wave_3_input == "-") return;
+
+    console.log(wave_3_input);
+
+    wave_3.frequency = custom_wave_note_to_memory_value[wave_3_input.substring(0, 3)];
+    wave_3.volume = parseInt(wave_3_input[3], 4);
+    wave_3.use_length = parseInt(wave_3_input[4], 2);
+    wave_3.sound_length_counter = parseInt(Number(wave_3_input[5] + "" + wave_3_input[6] + wave_3_input[7], 256) + 1);
+    wave_3.current_waveform = parseInt(wave_3_input[8], 16);
+
+}
+
+
+
+/* key 
+volume: 0-f
+envelope_direction: 0-1 (down/up)
+envelope: 0-f
+use_length: 0 - 1
+length: 01 - 64
+noise_preset: 00-63
+small_shit_register: 0-1
+*/
+var interpret_channel_4 = function(){
+    noise_input = patterns[4][current_patterns[4]][current_step]
+    if (noise_input == "-") return;
+
+  noise_4.volume = parseInt(noise_input[0], 16);
+  console.log(noise_4.volume);
+  noise_4.envelope_direction = parseInt(noise_input[1], 2);
+  noise_4.envelope_number = parseInt(noise_input[2], 16);
+  noise_4.use_length = parseInt(noise_input[3], 2);
+  console.log (parseInt(Number(noise_input[4] + "" + noise_input[5]))+1);
+  noise_4.sound_length_counter = parseInt(Number(noise_input[4] + "" + noise_input[5])+1);
+  noise_4.lfsr_bit_width = parseInt(noise_input[6], 2);
+  noise_4.dividing_ratio = Number(0);                               // special set-up
+  noise_4.shift_clock_frequency = Number(4);                        // special set-up
+  if (noise_4.dividing_ratio == 0) {noise_4.dividing_ratio = 0.5;}
+
+  noise_4.noise_player.playbackRate.value = 11.8886167801 / noise_4.dividing_ratio /
+    Math.pow(2, noise_4.shift_clock_frequency);
+
+
+    }
---
+++ pattern_layout/pattern_control_initialize.js
@@ -0,0 +1,130 @@
+var initialize = false;
+var custom_waveforms = [saw, triangle, square];
+var fill_table = function(current_pattern) {
+    if (initialize){
+        current_patterns[1] = document.getElementById('pattern_1').value;
+        current_patterns[2] = document.getElementById('pattern_2').value;
+        current_patterns[3] = document.getElementById('pattern_3').value;
+        current_patterns[4] = document.getElementById('pattern_4').value;
+    }
+    initialize = true
+
+    var table_header = "<tr><td>Pulse 1"+
+    "<select id='pattern_1' onchange='fill_table()' value="+current_patterns[1]+"></select>"+
+    "</td><td>Pulse 2"+
+    "<select id='pattern_2' onchange='fill_table()' value="+current_patterns[1]+"></select>"+
+    "</td><td>Waveform"+
+    "<select id='pattern_3' onchange='fill_table()' value="+current_patterns[1]+"></select>"+
+    "</td><td>Noise"+
+    "<select id='pattern_4' onchange='fill_table()' value="+current_patterns[1]+"></select>"+
+    "</td></tr>";
+
+
+
+    var table_contents = "";
+    for (var j = 0; j < 32; j++){
+        table_contents = table_contents + "<tr id=row_" + j + ">" + 
+        "<td><input type='text' onchange='setValue(1, "+j+")' id='ch1_" + j + "' value='" + patterns[1][current_patterns[1]][j] + "'></input></td>"+
+        "<td><input type='text' onchange='setValue(2, "+j+")' id='ch2_" + j + "' value='" + patterns[2][current_patterns[2]][j] + "'></input></td>"+
+        "<td><input type='text' onchange='setValue(3, "+j+")' id='ch3_" + j + "' value='" + patterns[3][current_patterns[3]][j] + "'></input></td>"+
+        "<td><input type='text' onchange='setValue(4, "+j+")' id='ch4_" + j + "' value='" + patterns[4][current_patterns[4]][j] + "'></input></td>"+
+        "</tr>"
+    }
+    document.getElementById('control_table').innerHTML = table_header + table_contents;
+        
+    var selection = "";
+    for (var i = 0; i < 36; i++){
+        selection += "<option value='"+patterns_available[i]+"'>"+patterns_available[i]+"</option>";
+    }
+    document.getElementById('pattern_1').innerHTML = selection;
+    document.getElementById('pattern_2').innerHTML = selection;
+    document.getElementById('pattern_3').innerHTML = selection;
+    document.getElementById('pattern_4').innerHTML = selection;
+    document.getElementById('pattern_1').value = current_patterns[1];
+    document.getElementById('pattern_2').value = current_patterns[2];
+    document.getElementById('pattern_3').value = current_patterns[3];
+    document.getElementById('pattern_4').value = current_patterns[4];
+    
+}
+var setValue = function(channel, note){
+        var new_note = (document.getElementById("ch" +channel+"_" +  note).value);
+        document.getElementById("ch4_" +  current_step).innerHTML = new_note;
+        console.log(channel, note
+        , document.getElementById("ch" +channel+"_" +  note).value);
+        patterns[channel][current_patterns[channel]][note] = new_note;
+}
+
+
+var tempo;
+var playing_interval;
+var play_pattern = function(){
+    window.clearTimeout(playing_interval)
+    pattern_step = 0
+    current_step = 0; //setting this up for visualyzing either
+    tempo = document.getElementById('tempo').value;
+    playing_interval = setInterval(function(){
+        document.getElementById('row_' + current_step).style.backgroundColor = "";
+        document.getElementById('row_' + current_step).style.color = "";
+
+        interpret_channel_1(); 
+        interpret_channel_2(); 
+        interpret_channel_3(); 
+        interpret_channel_4(); 
+
+        current_step = ++current_step % 32;
+
+        if ( current_page == "pattern_layout" && current_step == 0)
+        {
+        fill_table()
+        document.getElementById('row_pl_' + pattern_step).style.backgroundColor = "";
+        document.getElementById('row_pl_' + pattern_step).style.color = "";
+            console.log(pattern_step)
+            pattern_step = ++pattern_step % patterns_used;
+            current_patterns[1] = pattern_layout[1][pattern_step]
+            current_patterns[2] = pattern_layout[2][pattern_step]
+            current_patterns[3] = pattern_layout[3][pattern_step]
+            current_patterns[4] = pattern_layout[4][pattern_step]
+        console.log(document.getElementById('row_pl_' + pattern_step).style.backgroundColor)
+        document.getElementById('row_pl_' + pattern_step).style.backgroundColor = "#87678c";
+        document.getElementById('row_pl_' + pattern_step).style.color = "white";
+        }
+
+
+
+        document.getElementById('row_' + current_step).style.backgroundColor = "#87678c";
+        document.getElementById('row_' + current_step).style.color = "white";
+
+    }, 1000 / ( tempo / 60))
+    
+}
+var stop_playing = function(){
+    window.clearTimeout(playing_interval)
+}
+
+
+var current_patterns = [1,1,1,1,1];
+
+var patterns_available = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+                          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+                          's', 't', 'u', 'w', 'x', 'y', 'z' ] 
+
+var patterns = []
+        patterns[1] = {}
+        patterns[2] = {}
+        patterns[3] = {}
+        patterns[4] = {}
+
+    for (var j = 1; j < 5; j++){
+        for (var i = 0; i < 34; i++){
+        patterns[j][patterns_available[i]] = [];
+            for (var k=0; k < 32; k++)
+                patterns[j][patterns_available[i]][k] = '-' ;//15; //patterns_available[i] + k
+        }
+    }
+
+fill_table();
+
+
+var current_step = 0; //setting this up for visualyzing either
+var selection = "";
